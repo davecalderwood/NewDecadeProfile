f8ed9951a1fef21d188b1612a06721622841dc54
Dave Calderwood <davidhcalderwood@gmail.com>
2020-01-03 15:09:37 -0500

    added paged components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageOneComponent } from './pages/page-one/page-one.component';
+import { PageTwoComponent } from './pages/page-two/page-two.component';
+import { PageThreeComponent } from './pages/page-three/page-three.component';
+import { PageFourComponent } from './pages/page-four/page-four.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageOneComponent,
+    PageTwoComponent,
+    PageThreeComponent,
+    PageFourComponent
   ],
   imports: [
     BrowserModule,
